3116baa7290838627fa6192b97ec9720adf9514f
Milos Vasic <milos85vasic@gmail.com>
2017-06-05 18:12:44 +0200

    Fundamental Kotlin - Second Edition: Example updates.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/syntax/Values.kt
@@ -0,0 +1,14 @@
+package net.milosvasic.fundamental.kotlin.syntax
+
+// Constants:
+// We provide type and the value:
+val x: Int = 1
+
+// Simplified: Int type is inferred
+val y = 1
+
+// Type is required when no initializer is provided:
+// val z: Int
+
+// Variable:
+var m: Int = 0
