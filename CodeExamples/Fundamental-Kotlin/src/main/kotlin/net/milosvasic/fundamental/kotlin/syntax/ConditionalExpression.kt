eda893b22f7f9129120496078aa891585880f0ed
Milos Vasic <milos85vasic@gmail.com>
2017-06-06 18:38:45 +0200

    Fundamental Kotlin - Second Edition: String templates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/syntax/ConditionalExpression.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/syntax/ConditionalExpression.kt
@@ -1,14 +1,12 @@
 package net.milosvasic.fundamental.kotlin.syntax
 
-/**
- * Created by milosvasic on 6/4/16.
- */
-fun conditionalExpressionExample(x: Int, y: Int): Int {
-    if(x == y){
+fun compare(x: Int, y: Int): Int {
+    if (x > y) {
         return 1
-    } else {
-        return 0
+    } else if (x < y) {
+        return -1
     }
+    return 0
 }
 
-fun conditionalExpressionExample2(x: Int, y: Int) = if (x == y) 1 else 0
+fun compare2(x: Int, y: Int) = if (x > y) 1 else if (x < y) -1 else 0
