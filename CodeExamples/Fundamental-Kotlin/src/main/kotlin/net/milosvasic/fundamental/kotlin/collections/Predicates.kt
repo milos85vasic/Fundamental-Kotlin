173f207b0ed941cadc7c71332cc936f211f99e01
Milos Vasic <milos85vasic@gmail.com>
2017-06-16 10:30:48 +0200

    Fundamental Kotlin - Second Edition: Example updates.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/collections/Predicates.kt
@@ -0,0 +1,16 @@
+package net.milosvasic.fundamental.kotlin.collections
+
+
+val check: (Int) -> Boolean = { it > 0 }
+
+fun `has at least one positive` (items: List<Int>): Boolean {
+    return items.any(check)
+}
+
+fun `has all items positive` (items: List<Int>): Boolean {
+    return items.all(check)
+}
+
+fun `get the number of positive items` (items: List<Int>): Int {
+    return items.count(check)
+}
