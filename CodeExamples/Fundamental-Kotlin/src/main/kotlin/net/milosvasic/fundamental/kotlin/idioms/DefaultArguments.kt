a62e3b26c28d77942acdc39a1e54b7b6ef27c165
Milos Vasic <milos85vasic@gmail.com>
2017-06-20 13:17:06 +0200

    Fundamental Kotlin - Second Edition: Example updates.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/idioms/DefaultArguments.kt
@@ -0,0 +1,4 @@
+package net.milosvasic.fundamental.kotlin.idioms
+
+
+fun sum(x: Int = 0, y: Int = 2) = x + y
--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/idioms/DefaultFunctionValue.kt
+++
@@ -1,8 +0,0 @@
-package net.milosvasic.fundamental.kotlin.idioms
-
-/**
- * Created by mvasic on 6/5/16.
- */
-fun defaultValueExample(x: Int = 0, y: Int = 2){
-// Further implementation ...
-}
