ceae2c33a19fa633c47570167253abf5724a5c0f
Milos Vasic <milos85vasic@gmail.com>
2017-06-20 13:38:26 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/idioms/TravereMap.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/idioms/TravereMap.kt
@@ -1,8 +1,6 @@
 package net.milosvasic.fundamental.kotlin.idioms
 
-/**
- * Created by mvasic on 6/5/16.
- */
-fun traverseMapExample(map: Map<String, Int>) {
-    for ((key, value) in map) println("$key : $value")
+
+fun traverse(map: Map<String, Int>) {
+    for ((key, value) in map) println("[ $key ] : [ $value ]")
 }
