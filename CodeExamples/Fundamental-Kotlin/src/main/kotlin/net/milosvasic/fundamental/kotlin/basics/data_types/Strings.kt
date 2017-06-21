62b5dcbf0493f2524d911e74e9763159df0baea3
Milos Vasic <milos85vasic@gmail.com>
2017-06-21 13:48:59 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/data_types/Strings.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/data_types/Strings.kt
@@ -1,32 +1,22 @@
 package net.milosvasic.fundamental.kotlin.basics.data_types
 
-/**
- * Created by milosvasic on 6/7/16.
- */
-fun stringsExample(){
+val s1 = "Some string"
 
-    val s = "Some string"
+val s2 = "Some\tstring\n"
 
-    // Iterate:
-    val iterator = s.iterator()
-    while(iterator.hasNext()) println("Character: ${iterator.next()}")
-    println("- - - - - - - - - - - - - ")
-
-    // For loop:
-    for(c in s) println("Character: $c")
-    println("- - - - - - - - - - - - - ")
-
-}
-
-fun stringsExample2(){
-
-    val s1 = "Some string"
-    val s2 = "Some\tstring\n"
-    val s3 = """
-        Raw string example...
+val s3 = """
+        Raw string example ...
         We have multiline here!
     """
 
-    println("$s1\n$s2\n$s3")
-
+fun stringIterator(s: String) {
+    val iterator = s.iterator()
+    while(iterator.hasNext()) println("Character: ${iterator.next()}")
 }
+
+fun stringIterator2(s: String) {
+    for(c in s) println("Character: $c")
+}
+
+
+
