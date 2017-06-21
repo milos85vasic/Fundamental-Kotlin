d18231ae7e4aff41caec9f93a7c5031679fbe453
Milos Vasic <milos85vasic@gmail.com>
2017-06-21 13:01:14 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/data_types/Numbers.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/data_types/Numbers.kt
@@ -1,13 +1,10 @@
 package net.milosvasic.fundamental.kotlin.basics.data_types
 
-/**
- * Created by milosvasic on 6/7/16.
- */
-fun playWithNumbers() {
-    val a: Int = 1 // a is boxed Int (java.lang.Integer)
+fun numbers() {
+    val a: Int = 1 // A is boxed Int (java.lang.Integer)
     var b: Long? = null // We can't pass b directly to a!
     b = a.toLong() // We must convert it!
     println("a: $a") // prints 1
     println("b: $b") // prints 1
-    print(a.equals(b)) // We compare them
+    print(a == b.toInt()) // We compare them
 }
