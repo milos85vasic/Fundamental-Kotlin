4506ba849d9789c69ee3b2572210e99c2645737a
Milos Vasic <milos85vasic@gmail.com>
2017-06-23 09:07:59 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/controll_flow/If.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/controll_flow/If.kt
@@ -1,15 +1,12 @@
 package net.milosvasic.fundamental.kotlin.basics.controll_flow
 
-/**
- * Created by milosvasic on 6/7/16.
- */
-fun ifExample(x: Int, y: Int) {
+fun check(x: Int, y: Int) {
     val result = if (x >= y) {
-        println("Condition ok.")
+        println("x >= y")
         true
     } else {
-        println("Condition else.")
+        println("x is not >= y")
         false
     }
-    println("Result $result")
+    println("Result [ $result ]\n- - - - - - - -")
 }
