6448d96af15553101a696655a70a868c85311bdb
Milos Vasic <milos85vasic@gmail.com>
2017-06-23 09:12:28 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/controll_flow/For.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/basics/controll_flow/For.kt
@@ -1,10 +1,7 @@
 package net.milosvasic.fundamental.kotlin.basics.controll_flow
 
-/**
- * Created by milosvasic on 6/8/16.
- */
-fun forExample(array: Array<String>) {
-    for ((index, value) in array.withIndex()) {
+fun members(members: Array<String>) {
+    for ((index, value) in members.withIndex()) {
         println("[ $index ][ $value ]")
     }
 }
