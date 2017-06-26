ca95594e2a496829742e2d85c41b351cafcdf1e0
Milos Vasic <milos85vasic@gmail.com>
2017-06-26 17:35:03 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/ClassExamples.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/ClassExamples.kt
@@ -1,10 +1,5 @@
 package net.milosvasic.fundamental.kotlin.classes
 
-import java.time.Year
-
-/**
- * Created by milosvasic on 6/8/16.
- */
 // Simple class definition
 class Dummy {}
 
@@ -15,22 +10,16 @@ class Car constructor(val brand: String)
 
 class Plane(val brand: String)
 
-class Calculator(val parameter: Int) {
-    val calcualtedValue: Int
-
-    init {
-        calcualtedValue = parameter * 2
-    }
-}
+class Calculator(val parameter: Int, val calculatedValue: Int = parameter * 2)
 
 class NotPublicConstructed private constructor(val name: String){
     // ...
 }
 
 class Person(val name: String) {
-    constructor(name: String, year: Int) : this(name) {
-    }
 
-    constructor(name: String, year: Int, height: Int) : this(name) {
-    }
+    constructor(name: String, year: Int) : this(name)
+
+    constructor(name: String, year: Int, height: Int) : this(name)
+
 }
