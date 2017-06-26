b08acc182c03235f2aabe23a811c9c49730de235
Milos Vasic <milos85vasic@gmail.com>
2017-06-26 17:49:42 +0200

    Fundamental Kotlin - Second Edition: Example updates.

--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/GetterSetter.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/GetterSetter.kt
@@ -1,8 +1,6 @@
 package net.milosvasic.fundamental.kotlin.classes
 
-/**
- * Created by milosvasic on 6/8/16.
- */
+
 class GetterSetter {
     var x: Int
         get() = x
--- CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/LateInit.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/kotlin/net/milosvasic/fundamental/kotlin/classes/LateInit.kt
@@ -1,15 +1,12 @@
 package net.milosvasic.fundamental.kotlin.classes
 
-/**
- * Created by milosvasic on 6/8/16.
- */
 class SomeExampleClass {
     fun printMe() {
         println("I am late initialized!")
     }
 }
 
-class TryLateInit {
+class Late {
     lateinit var x: SomeExampleClass
 
     fun printMe() {
