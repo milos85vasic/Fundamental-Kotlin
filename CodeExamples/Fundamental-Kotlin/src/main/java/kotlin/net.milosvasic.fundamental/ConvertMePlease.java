f9e49d7a1648f02c2d49848b18714fc77a1c8267
Milos Vasic <milos85vasic@gmail.com>
2020-07-19 15:36:10 +0200

    Fundamental Kotlin - Formatting.

--- CodeExamples/Fundamental-Kotlin/src/main/java/kotlin/net.milosvasic.fundamental/ConvertMePlease.java
+++ CodeExamples/Fundamental-Kotlin/src/main/java/kotlin/net.milosvasic.fundamental/ConvertMePlease.java
@@ -1,14 +1,11 @@
 package kotlin.net.milosvasic.fundamental;
 
-/**
- * Created by milosvasic on 6/4/17.
- */
 public class ConvertMePlease {
 
     private String myFieldToBEConverted;
 
     public ConvertMePlease(String myFieldToBEConverted) {
+
         this.myFieldToBEConverted = myFieldToBEConverted;
     }
-
 }
