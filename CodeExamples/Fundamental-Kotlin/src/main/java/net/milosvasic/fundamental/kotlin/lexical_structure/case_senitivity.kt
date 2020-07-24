c54dc8842fb158500f0d5808fab44c62e40b08a0
Milos Vasic <milos85vasic@gmail.com>
2020-07-24 15:37:43 +0200

    Fundamental Kotlin - Case sensitivity.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/lexical_structure/case_senitivity.kt
@@ -0,0 +1,11 @@
+
+val myConstant = 0
+val myCONSTANT = 1
+val MYconstant = 2
+
+fun main() {
+
+    println("myConstant = $myConstant")
+    println("myCONSTANT = $myCONSTANT")
+    println("MYconstant = $MYconstant")
+}
