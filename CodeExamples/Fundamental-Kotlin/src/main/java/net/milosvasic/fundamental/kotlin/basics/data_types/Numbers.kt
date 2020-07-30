096c9f1094247907cd5098c7d3a291c97d85fcfc
Milos Vasic <milos85vasic@gmail.com>
2020-07-30 16:21:31 +0200

    Fundamental Kotlin - Numbers.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/data_types/Numbers.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/data_types/Numbers.kt
@@ -1,10 +1,22 @@
 package net.milosvasic.fundamental.kotlin.basics.data_types
 
-fun numbers() {
-    val a: Int = 1 // A is boxed Int (java.lang.Integer)
-    var b: Long? = null // We can't pass b directly to a!
-    b = a.toLong() // We must convert it!
-    println("a: $a") // prints 1
-    println("b: $b") // prints 1
-    print(a == b.toInt()) // We compare them
+fun underscoredNumbers() {
+
+    val millions = 5_000_000
+    val hexBytes = 0xFF_EE_FF_EE
+    val bigOne = 1111_2222_3333_4444L
+    val bytes = 0b11001100_11001100_11001100_11001100
 }
+
+fun numbers() {
+
+    val a = 1 // "Int" number type
+    val b = a.toLong() // Convert "Int" into "Long"
+
+    println("a: $a") // prints: 1
+    println("b: $b") // prints: 1
+    print(a == b.toInt()) // We compare "a" and "b", it prints: true,
+                          // which is Boolean data type.
+}
+
+fun main() = numbers()
