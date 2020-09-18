26741a524d5c7ff3592c8510efb8489494ff2e45
Milos Vasic <milos85vasic@gmail.com>
2020-09-18 15:02:11 +0200

    Fundamental Kotlin - Blocks.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/Blocks.kt
@@ -0,0 +1,11 @@
+package net.milosvasic.fundamental.kotlin.basics
+
+fun main(args: Array<String>) { // <- Main function block start
+
+    if (args.size == 2) { // <-Start of 'if' block
+
+        // Statements:
+        print("1st argument: ${args[0]}")
+        print("2nd argument: ${args[0]}")
+    } // <- End of 'if' block
+} // <- Main function block end
