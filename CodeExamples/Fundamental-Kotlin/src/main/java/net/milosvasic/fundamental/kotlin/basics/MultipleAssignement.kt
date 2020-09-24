e8ab22ebd1f9f8bbc586bf9706cc7885614faae9
Milos Vasic <milos85vasic@gmail.com>
2020-09-24 15:18:48 +0200

    Fundamental Kotlin - Multiple assignment.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/MultipleAssignement.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/MultipleAssignement.kt
@@ -1,9 +1,22 @@
 package net.milosvasic.fundamental.kotlin.basics
 
+fun parameters(): List<Int> {
+
+    return listOf(2, 4, 6)
+}
+
 fun main() {
 
     val (first, second) = arrayOf(1, 2)
 
     println("First: $first")
     println("Second: $second")
+
+    fun parameters(): List<Int> {
+
+        return listOf(3, 5, 7)
+    }
+
+    val (parameter1, parameter2, parameter3) = parameters()
+    println("Parameters: $parameter1, $parameter2, $parameter3")
 }
