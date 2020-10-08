c25ab55f7e5a89b317455fd09a629c082aad88a9
Milos Vasic <milos85vasic@gmail.com>
2020-10-08 11:39:11 +0200

    Fundamental Kotlin - Arithmetics operators.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/operators/Arithmetic.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/operators/Arithmetic.kt
@@ -2,7 +2,20 @@ package net.milosvasic.fundamental.kotlin.basics.operators
 
 fun main(){
 
+    val a = 1
+    val b = 2
+    val c = 3
+    val hello = "Hello"
+    val world = "World"
+
     // "+" - Addition:
+    val sum = a + b + c
+    println("a + b + c = $sum")
+
+    val concatenation = hello + " " + world
+    // or converted into the template:
+    // concatenation = "$hello $world"
+    println(concatenation)
 
     // "-" - Subtraction:
 
