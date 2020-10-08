9454edde044b4ead45b1c0f59f29a99c74748433
Milos Vasic <milos85vasic@gmail.com>
2020-10-08 11:34:34 +0200

    Fundamental Kotlin - Arithmetics operators.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/operators/Arithmetic.kt
@@ -0,0 +1,14 @@
+package net.milosvasic.fundamental.kotlin.basics.operators
+
+fun main(){
+
+    // "+" - Addition:
+
+    // "-" - Subtraction:
+
+    // "*" - Multiplication:
+
+    // "/" - Division:
+
+    // "%" - Modulus:
+}
