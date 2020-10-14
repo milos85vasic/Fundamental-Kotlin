b363c1eb732540251dce9cdaa46b66323b997b6a
Milos Vasic <milos85vasic@gmail.com>
2020-10-14 13:13:55 +0200

    Fundamental Kotlin - Comparison.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/operators/Comparison.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/operators/Comparison.kt
@@ -4,8 +4,9 @@ fun main() {
 
     val a = 1
     val b = 2
-    val c = 2
+    val c = 1
 
+    println("a: $a, b: $b, c: $c")
     println("a == b: ${a == b}")
     println("a == c: ${a == c}")
     println("a != b: ${a != b}")
