bc8209f25d7e5a756c88e98717297abcefe0fb82
Milos Vasic <milos85vasic@gmail.com>
2020-11-03 12:31:55 +0100

    Fundamental Kotlin - Vararg functions.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/functions/Vararg.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/functions/Vararg.kt
@@ -11,9 +11,26 @@ fun main() {
         println("Sum: $sum")
     }
 
+    fun sumAndMultiply(multiply: Int, vararg args: Int) {
+
+        var sum = 0
+        for (x in args) {
+            sum += x
+        }
+        val result = sum * multiply
+        println("Sum and multiply: $result")
+    }
+
     sum()
     sum(1)
     sum(1, 2)
     sum(1, 2, 3)
     sum(1, 2, 3, 4)
+
+    val multiply = 100
+    sumAndMultiply(multiply)
+    sumAndMultiply(multiply, 1)
+    sumAndMultiply(multiply, 1, 2)
+    sumAndMultiply(multiply, 1, 2, 3)
+    sumAndMultiply(multiply, 1, 2, 3, 4)
 }
