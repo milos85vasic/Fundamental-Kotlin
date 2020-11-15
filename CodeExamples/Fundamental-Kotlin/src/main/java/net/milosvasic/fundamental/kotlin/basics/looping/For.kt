c00fd073614c4991adcb3914e481cf3854b1c813
Milos Vasic <milos85vasic@gmail.com>
2020-11-15 15:02:41 +0100

    Fundamental Kotlin - For looping.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/looping/For.kt
@@ -0,0 +1,37 @@
+package net.milosvasic.fundamental.kotlin.basics.looping
+
+fun main() {
+
+    fun counter(to: Int) {
+
+        for (x in 0..to) {
+            println("[ $x ]")
+        }
+    }
+
+    fun cars(cars: Collection<String>) {
+
+        for (c in cars.indices) {
+            // Do something with index of an element.
+        }
+    }
+
+    fun clubs(clubs: Collection<String>) {
+
+        for (c in clubs) println("Club [ ${c.capitalize()} ]")
+    }
+
+    fun clubs2(clubs: Collection<String>) {
+
+        clubs.forEach { c ->
+            println("Club [ ${c.capitalize()} ]")
+        }
+    }
+
+    fun players(players: Collection<String>) {
+
+        players.forEachIndexed { index, item ->
+            println("[ $index ] ${item.capitalize()}")
+        }
+    }
+}
