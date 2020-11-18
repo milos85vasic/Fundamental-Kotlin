73f579f22250f61a0c4317f3e443942dd71a91d1
Milos Vasic <milos85vasic@gmail.com>
2020-11-18 14:02:05 +0100

    Fundamental Kotlin - Ranges.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/looping/Ranges.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/looping/Ranges.kt
@@ -7,12 +7,22 @@ fun main() {
         for (x in 0..to) println("x: $x")
     }
 
+    fun reverseRange(to: Int) {
+
+        for (x in to downTo 0) println("x: $x")
+    }
+
     fun checkInRange(what: Int, to: Int) {
 
-        if (what !in 0..to) println("No $what is not in range between 0 and $to")
+        if (what in 0..to) {
+            println("$what is in range between 0 and $to")
+        } else {
+            println("$what is NOT in range between 0 and $to")
+        }
     }
 
     range(5)
+    reverseRange(5)
 
     for (x in -5..10) {
 
