647c1edcb79305f7a6cf5a79b6f82d9adaf55a5b
Milos Vasic <milos85vasic@gmail.com>
2020-12-13 13:49:29 +0100

    Fundamental Kotlin - Cleaning up.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/collections/FilterMap.kt
+++
@@ -1,9 +0,0 @@
-package net.milosvasic.fundamental.kotlin.collections
-
-fun filterItems(items: List<Int>) : List<Int> {
-    return items.filter { it % 2 == 0 }
-}
-
-fun filterMapItems(items: List<Int>) : List<Int> {
-    return items.map { it + 1000 }
-}
