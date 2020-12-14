cdbcbd83ffec0151db5853a5a735145615d95dd9
Milos Vasic <milos85vasic@gmail.com>
2020-12-14 13:58:11 +0100

    Fundamental Kotlin - Predicates.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Predicates.kt
@@ -0,0 +1,19 @@
+package net.milosvasic.fundamental.kotlin.basics.collections
+
+
+fun main() {
+
+    val check: (Int) -> Boolean = { it > 0 }
+
+    fun `has at least one positive`(items: List<Int>): Boolean {
+        return items.any(check)
+    }
+
+    fun `has all items positive`(items: List<Int>): Boolean {
+        return items.all(check)
+    }
+
+    fun `number of positive items`(items: List<Int>): Int {
+        return items.count(check)
+    }
+}
