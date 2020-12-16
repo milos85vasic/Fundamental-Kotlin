a8677623182d0310750078f3f035582dcfd09da9
Milos Vasic <milos85vasic@gmail.com>
2020-12-16 11:29:41 +0100

    Fundamental Kotlin - Map.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Map.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Map.kt
@@ -2,5 +2,11 @@ package net.milosvasic.fundamental.kotlin.basics.collections
 
 fun main() {
 
-    // TODO:
+    val numbers = listOf(0, 1, 2, 3, 4, 5)
+    val squares = numbers.map { it * it }
+
+    numbers.forEachIndexed { index, it ->
+
+        println("Square of $it is: ${squares[index]}")
+    }
 }
