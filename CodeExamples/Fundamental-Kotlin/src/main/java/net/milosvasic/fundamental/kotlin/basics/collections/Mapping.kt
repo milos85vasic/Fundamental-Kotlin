ee2e70fe4f86f0efe73443697a168fbc884877fe
Milos Vasic <milos85vasic@gmail.com>
2020-12-17 13:04:21 +0100

    Fundamental Kotlin - Mapping.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Mapping.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Mapping.kt
@@ -2,5 +2,28 @@ package net.milosvasic.fundamental.kotlin.basics.collections
 
 fun main() {
 
+    class Vehicle(val name: String)
 
+    val cars = listOf(
+
+        Vehicle("Bmw"),
+        Vehicle("Mercedes"),
+        Vehicle("Toyota")
+    )
+
+    val busses = listOf(
+
+        Vehicle("Solaris"),
+        Vehicle("Champion"),
+        Vehicle("Ikarbus")
+    )
+
+    val vehicles = listOf(cars, busses)
+    val manufacturers = vehicles.flatMap {
+        it
+    }.map {
+        it.name
+    }
+
+    println(manufacturers)
 }
