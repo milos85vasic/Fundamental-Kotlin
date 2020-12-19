15964948b9e60be38a29c437e9a7ae714197e0d5
Milos Vasic <milos85vasic@gmail.com>
2020-12-19 15:27:25 +0100

    Fundamental Kotlin - Sort.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Sort.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Sort.kt
@@ -3,6 +3,14 @@ package net.milosvasic.fundamental.kotlin.basics.collections
 fun main(){
 
     val numbers = listOf(1, 8, 9, 4, 5, 22, 44, 645, 67).sorted()
-    val stringNumbers = listOf("1", "-3", "5").sortedBy { it.toInt() }
+    println("Numbers: $numbers")
 
+    val stringNumbers = listOf("1", "-3", "5").sortedBy { it.toInt() }
+    println("String numbers: $stringNumbers")
+
+    val userIds = mapOf("admin" to 0, "john.smith" to 35653, "john.doe" to 32455)
+    println("User IDs: $userIds")
+
+    val sortedUserIds = userIds.toSortedMap()
+    println("Sorted user IDs: $sortedUserIds")
 }
