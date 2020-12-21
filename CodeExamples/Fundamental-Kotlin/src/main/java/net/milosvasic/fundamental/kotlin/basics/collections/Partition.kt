533746acd5f7ce71cb375a4f91144962b56a37ca
Milos Vasic <milos85vasic@gmail.com>
2020-12-21 16:27:22 +0100

    Fundamental Kotlin - Partition.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/collections/Partition.kt
@@ -0,0 +1,10 @@
+package net.milosvasic.fundamental.kotlin.basics.collections
+
+fun main() {
+
+
+    val itemsToPartition = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val partitionedItems = itemsToPartition.partition { it % 2 == 0 }
+
+
+}
