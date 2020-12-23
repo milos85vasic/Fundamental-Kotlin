c199d29626836557b6a77d5e37e232d41fa9479d
Milos Vasic <milos85vasic@gmail.com>
2020-12-23 13:16:12 +0100

    Fundamental Kotlin - Strong references.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/Strong.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/Strong.kt
@@ -4,6 +4,9 @@ fun main() {
 
     data class Wrapper(val what: Any)
 
-    val number = 1
+    val number = 1                   // <- Default (strong) reference
     val wrapper = Wrapper(100) // <- Default (strong) reference
+
+    var number2: Int? = null         // <- Default (strong) reference
+    var wrapper2: Wrapper? = null    // <- Default (strong) reference
 }
