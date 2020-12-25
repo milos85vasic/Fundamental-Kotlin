61ea92d7839826b0ffda5eafdd3bd60cd0223c8c
Milos Vasic <milos85vasic@gmail.com>
2020-12-25 14:52:44 +0100

    Fundamental Kotlin - Static field leak.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/StaticFieldLeak.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/StaticFieldLeak.kt
@@ -1,6 +1,26 @@
 package net.milosvasic.fundamental.kotlin.basics.references.leaks
 
+val staticList = mutableListOf<Double>()
+
 fun main() {
 
+    fun populate() {
+        for (x in 0 until 100 * 1000 * 1000) {
+            staticList.add(x.toDouble())
+        }
+    }
 
+    var memory = Runtime.getRuntime().freeMemory()
+    println("Free memory (1): $memory")
+
+    println("Populating static list")
+    populate()
+    memory = Runtime.getRuntime().freeMemory()
+    println("Free memory (2): $memory")
+
+    println("Executing garbage collection")
+    Runtime.getRuntime().gc()
+
+    memory = Runtime.getRuntime().freeMemory()
+    println("Free memory (3): $memory")
 }
