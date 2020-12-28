490bfe3204279d5ce89ef3cf4de47fbfa18cbdc9
Milos Vasic <milos85vasic@gmail.com>
2020-12-28 16:14:28 +0100

    Fundamental Kotlin - Override leak.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/OverrideLeak.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/OverrideLeak.kt
@@ -2,7 +2,13 @@ package net.milosvasic.fundamental.kotlin.basics.references.leaks
 
 fun main() {
 
-    class Vehicle(var model: String)
+    class Wrapper(var data: String)
 
+    val vehicles = mutableMapOf<Wrapper, Int>()
 
+    val word = "Kotlin"
+    for (x in 0 until 100) {
+
+        vehicles[Wrapper(word)] = x
+    }
 }
