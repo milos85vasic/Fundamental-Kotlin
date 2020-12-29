94361278d9474a9c67ba6c20f3127d4ac7ded93e
Milos Vasic <milos85vasic@gmail.com>
2020-12-29 12:58:59 +0100

    Fundamental Kotlin - Inner class leak.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/InnerClassLeak.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/references/leaks/InnerClassLeak.kt
@@ -2,5 +2,18 @@ package net.milosvasic.fundamental.kotlin.basics.references.leaks
 
 fun main() {
 
+    class Wrapper(private var data: String) {
 
+        inner class Inner {
+
+            fun print() {
+
+                // We are accessing to enclosing class reference and its field:
+                println("Data: $data")
+            }
+        }
+    }
+
+    val wrapper = Wrapper("Kotlin")
+    wrapper.Inner().print()
 }
