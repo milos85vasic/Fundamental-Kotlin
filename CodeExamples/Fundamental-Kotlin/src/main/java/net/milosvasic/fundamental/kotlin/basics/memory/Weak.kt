9d3273f21ec7d84f751286229982c2e01eab652d
Milos Vasic <milos85vasic@gmail.com>
2021-01-02 14:27:15 +0100

    Fundamental Kotlin - Weak reference.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/memory/Weak.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/memory/Weak.kt
@@ -1,6 +1,27 @@
 package net.milosvasic.fundamental.kotlin.basics.memory
 
+import java.lang.ref.WeakReference
+
 fun main() {
 
+    // Referent definition, some 'dummy' class:
+    class Dummy {
 
+        // Referent's method:
+        fun hello() = println("Dummy: $this")
+    }
+
+    // Referent:
+    val dummy = Dummy()
+
+    // We are initializing weak reference
+    // by passing a referent as a parameter:
+    val weak = WeakReference(dummy)
+
+    weak.get()?.hello()
+
+    // Release weak reference:
+    weak.clear()
+
+    weak.get()?.hello()
 }
