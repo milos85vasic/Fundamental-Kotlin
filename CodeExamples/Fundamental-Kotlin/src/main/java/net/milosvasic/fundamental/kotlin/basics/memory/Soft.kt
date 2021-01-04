7a39418df9c57693318fce2c0a6e59ad0cd18598
Milos Vasic <milos85vasic@gmail.com>
2021-01-04 14:07:12 +0100

    Fundamental Kotlin - Soft reference.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/memory/Soft.kt
@@ -0,0 +1,25 @@
+package net.milosvasic.fundamental.kotlin.basics.memory
+
+import java.lang.ref.SoftReference
+
+
+fun main() {
+
+    class Dummy {
+
+        fun hello() = println("Dummy: ${hashCode()}")
+    }
+
+    val dummy = Dummy()
+
+    // We are initializing soft reference
+    // by passing a referent as a parameter:
+    val soft = SoftReference(dummy)
+
+    soft.get()?.hello()
+
+    // Release soft reference:
+    soft.clear()
+
+    soft.get()?.hello()
+}
