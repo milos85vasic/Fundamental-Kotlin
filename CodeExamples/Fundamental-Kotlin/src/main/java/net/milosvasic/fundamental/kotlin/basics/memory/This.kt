cc0e997cf003254253c4527d4f6265705eea22fb
Milos Vasic <milos85vasic@gmail.com>
2021-01-06 11:21:28 +0100

    Fundamental Kotlin - This reference.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/memory/This.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/memory/This.kt
@@ -2,5 +2,15 @@ package net.milosvasic.fundamental.kotlin.basics.memory
 
 fun main() {
 
+    class Wrapper(private val value: Int) {
 
+        fun sum(value: Int) {
+
+            val sum = this.value + value
+            println("Sum: $sum")
+        }
+    }
+
+    val wrapper = Wrapper(1)
+    wrapper.sum(3)
 }
