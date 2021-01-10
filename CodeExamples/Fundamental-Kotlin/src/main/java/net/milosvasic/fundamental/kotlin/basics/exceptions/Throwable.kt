1cc3a14a15bdc5bfef0c8408176f01dae8eb25ef
Milos Vasic <milos85vasic@gmail.com>
2021-01-10 14:58:10 +0100

    Fundamental Kotlin - Throwable.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/exceptions/Throwable.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/exceptions/Throwable.kt
@@ -1,6 +1,33 @@
 package net.milosvasic.fundamental.kotlin.basics.exceptions
 
+import kotlin.system.exitProcess
+
 fun main() {
 
+    fun fail(t: Throwable) {
 
+        println(t.message)
+        exitProcess(1)
+    }
+
+    @Throws(Throwable::class)
+    fun process(what: Int) {
+
+        if (what < 0) {
+
+            throw Throwable("Error")
+        }
+        println("Processing: $what")
+    }
+
+    val items = listOf(2, 4, 6, 0, -2, -4, -6)
+    items.forEach {
+        try {
+
+            process(it)
+        } catch (t: Throwable) {
+
+            fail(t)
+        }
+    }
 }
