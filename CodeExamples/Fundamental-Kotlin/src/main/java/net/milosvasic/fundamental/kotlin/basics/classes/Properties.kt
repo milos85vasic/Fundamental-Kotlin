8d08cb7eb9ac61b624d6aff53b851d16387bd15f
Milos Vasic <milos85vasic@gmail.com>
2021-01-16 13:11:56 +0100

    Fundamental Kotlin - Class properties.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Properties.kt
@@ -0,0 +1,22 @@
+package net.milosvasic.fundamental.kotlin.basics.classes
+
+fun main() {
+
+    class Coordinate {
+
+        var x: Int = 0
+        var y: Int = 0
+        var z: Int = 0
+
+        fun print() = println("Coordinate(x=$x, y=$y, z=$z)")
+    }
+
+    val center = Coordinate()
+    center.print()
+
+    val location = Coordinate()
+    location.x = 10
+    location.y = 20
+    location.z = 30
+    location.print()
+}
--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/classes/Properties.kt
+++
@@ -1,8 +0,0 @@
-package net.milosvasic.fundamental.kotlin.classes
-
-class PropertyOwner {
-    var x: Int = 0
-    var y: Int? = null
-    val z: Int = 1
-}
-
