54f5608a4d27a90475e27817a752dbe68e30c754
Milos Vasic <milos85vasic@gmail.com>
2021-01-19 14:47:33 +0100

    Fundamental Kotlin - Overriding.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Overriding.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Overriding.kt
@@ -47,7 +47,7 @@ fun main(){
 
         final override fun turnOn() {
 
-            // Execute something before 'super' business logic:
+            // Executing something before 'super' business logic:
             println("$model: Preparing")
             super.turnOn()
             println("$model: Engine is running")
