2fe4d1bc0f6a977d3dcdc5c13a23013ef7616893
Milos Vasic <milos85vasic@gmail.com>
2021-01-19 14:12:18 +0100

    Fundamental Kotlin - Overriding.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Overriding.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Overriding.kt
@@ -3,15 +3,43 @@ package net.milosvasic.fundamental.kotlin.basics.classes
 fun main(){
 
     open class Engine {
-        open fun turnOn() {
-            println("Turn on")
+
+        open fun turnOn() = println("Turn on")
+
+        open fun turnOff() = println("Turn off")
+    }
+
+    class CarEngine : Engine() {
+
+        override fun turnOn() {
+
+            super.turnOn()
+            println("Car is starting")
+        }
+
+        override fun turnOff() {
+
+            super.turnOff()
+            println("Car is stopping")
         }
     }
 
-    class SuperEngine : Engine() {
+    class CustomEngine : Engine() {
+
         override fun turnOn() {
-            super.turnOn()
-            println("Do a special thing!")
+
+            // No call for the 'super'
+            println("Car is starting")
         }
+
+        // We will not override 'turnOff' function for this class
     }
+
+    val carEngine = CarEngine()
+    carEngine.turnOn()
+    carEngine.turnOff()
+
+    val customEngine = CustomEngine()
+    customEngine.turnOn()
+    customEngine.turnOff()
 }
