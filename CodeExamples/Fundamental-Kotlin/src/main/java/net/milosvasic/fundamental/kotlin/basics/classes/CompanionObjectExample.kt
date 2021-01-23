e1a525f653a20db1558499ed095cb7f150c28bd4
Milos Vasic <milos85vasic@gmail.com>
2021-01-23 19:40:05 +0100

    Fundamental Kotlin - Class companion object.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/CompanionObjectExample.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/CompanionObjectExample.kt
@@ -1,9 +1,15 @@
-package net.milosvasic.fundamental.kotlin.classes
+package net.milosvasic.fundamental.kotlin.basics.classes
 
 class CompanionObjectExample {
+
     companion object StaticObject {
         fun iAmStatic(){
             println("I am static!")
         }
     }
+}
+
+fun main () {
+
+
 }
