1588782536c378f041c66ca4a46e325930779fe9
Milos Vasic <milos85vasic@gmail.com>
2021-01-24 17:50:28 +0100

    Fundamental Kotlin - Anonymous object.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/AnonymousObject.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/AnonymousObject.kt
@@ -4,60 +4,83 @@ package net.milosvasic.fundamental.kotlin.basics.classes
 fun main() {
 
     abstract class Command {
+
         abstract fun execute()
     }
 
     class Executor : Command() {
+
         private val commands = mutableListOf<Command>()
 
         fun addCommand(command: Command) {
+
             commands.add(command)
         }
 
         override fun execute() {
+
             for (command in commands) {
                 command.execute()
             }
         }
     }
 
+    // Let's try it:
     val executor = Executor()
 
-    executor.addCommand(object : Command() {
+    val obj = object : Command() {
+
         override fun execute() {
+
             println("Starting session.")
         }
-    })
+    }
+    executor.addCommand(obj)
 
-    executor.addCommand(object : Command() {
+    val obj2 = object : Command() {
+
         override fun execute() {
+
             println("Logging in.")
         }
-    })
+    }
+    executor.addCommand(obj2)
 
-    executor.addCommand(object : Command() {
+    val obj3 = object : Command() {
+
         override fun execute() {
+
             println("Launch app 1")
         }
-    })
+    }
+    executor.addCommand(obj3)
 
-    executor.addCommand(object : Command() {
+    val obj4 = object : Command() {
+
         override fun execute() {
+
             println("Launch app 2")
         }
-    })
+    }
+    executor.addCommand(obj4)
 
-    executor.addCommand(object : Command() {
+    val obj5 = object : Command() {
+
         override fun execute() {
+
             println("Logging out")
         }
-    })
+    }
+    executor.addCommand(obj5)
 
-    executor.addCommand(object : Command() {
+    val obj6 = object : Command() {
+
         override fun execute() {
+
             println("Stopping session")
         }
-    })
+    }
+    executor.addCommand(obj6)
 
     executor.execute()
 }
