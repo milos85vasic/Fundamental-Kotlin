e08f05eb9beacd3934b0bb0fa85f4ad3a97c259b
Milos Vasic <milos85vasic@gmail.com>
2021-01-25 11:58:38 +0100

    Fundamental Kotlin - Object.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Object.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/classes/Object.kt
@@ -1,6 +1,24 @@
 package net.milosvasic.fundamental.kotlin.basics.classes
 
+import java.awt.Image
+import java.net.URL
+
+object ImageManager {
+
+    private val images = mutableListOf<Image>()
+
+    fun download(url: URL): Boolean {
+
+        // Perform image download procedure
+        // and add to 'images' collection ...
+        return true
+    }
+
+    fun getImages() = images
+}
+
 fun main() {
 
-
+    val url = URL("https://fundamental-kotlin.com/images/cover.png")
+    ImageManager.download(url) // <-- Not instantiation needed for the 'ImageManager' data type
 }
