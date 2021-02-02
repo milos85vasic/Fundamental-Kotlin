6b39a45b07f73b89a2a6ce9549a27d148587f7cd
Milos Vasic <milos85vasic@gmail.com>
2021-02-02 16:13:34 +0100

    Fundamental Kotlin - Interface properties.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/object_oriented/InterfaceProperties.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/object_oriented/InterfaceProperties.kt
@@ -1,6 +1,43 @@
 package net.milosvasic.fundamental.kotlin.basics.object_oriented
 
+import java.net.URL
+
+interface Process {
+
+    val cores: Int
+
+    val memory: Int
+        get() = 64
+
+    fun execute()
+
+    fun getUsedResources() = "cores=$cores, memory=$memory"
+}
+
 fun main() {
 
+    class Download(val what: URL) : Process {
 
+        override val cores: Int
+            get() = 4
+
+        override fun execute() {
+
+            println("Downloading: $what, using(${getUsedResources()})")
+        }
+    }
+
+    class Encrypt(val what: String) : Process {
+
+        override val cores: Int
+            get() = 8
+
+        override val memory: Int
+            get() = 128
+
+        override fun execute() {
+
+            println("Encrypting: $what, using(${getUsedResources()})")
+        }
+    }
 }
