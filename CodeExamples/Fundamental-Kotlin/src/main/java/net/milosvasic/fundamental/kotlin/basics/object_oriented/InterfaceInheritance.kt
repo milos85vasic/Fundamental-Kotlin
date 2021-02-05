c02297839b9a3c851916e5c94e0edfd78127c7e8
Milos Vasic <milos85vasic@gmail.com>
2021-02-05 15:24:33 +0100

    Fundamental Kotlin - Interface inheritance.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/object_oriented/InterfaceInheritance.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/object_oriented/InterfaceInheritance.kt
@@ -1,6 +1,41 @@
 package net.milosvasic.fundamental.kotlin.basics.object_oriented
 
+interface Device {
+
+    val model: String
+}
+
+interface AudioDevice : Device {
+
+    val brand: String
+    val serialNumber: Long
+
+    override val model: String
+        get() = "$brand::$serialNumber"
+
+    fun play()
+}
+
+class MusicPlayer(
+    override val brand: String,
+    override val serialNumber: Long
+) : AudioDevice {
+
+    override fun play() {
+
+        println("'$model' is playing")
+    }
+}
+
 fun main() {
 
-    
+    listOf(
+
+        MusicPlayer("Sony", 1241),
+        MusicPlayer("Panasonic", 1001001),
+        MusicPlayer("Sony", 1242)
+    ).forEach {
+
+        it.play()
+    }
 }
