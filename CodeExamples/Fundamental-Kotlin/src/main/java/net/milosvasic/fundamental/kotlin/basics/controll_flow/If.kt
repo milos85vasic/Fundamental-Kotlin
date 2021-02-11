d65a114009640c754d6ce789e9b37b5345efe73b
Milos Vasic <milos85vasic@gmail.com>
2021-02-11 15:12:18 +0100

    Fundamental Kotlin - Control Flow - If.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/controll_flow/If.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/basics/controll_flow/If.kt
@@ -1,12 +1,22 @@
 package net.milosvasic.fundamental.kotlin.basics.controll_flow
 
-fun check(x: Int, y: Int) {
-    val result = if (x >= y) {
-        println("x >= y")
-        true
-    } else {
-        println("x is not >= y")
-        false
+fun main() {
+
+    fun check(x: Int, y: Int) {
+
+        val result = if (x >= y) {
+
+            println("x >= y")
+            true
+        } else {
+
+            println("x is not >= y")
+            false
+        }
+        println("Result: $result")
     }
-    println("Result [ $result ]\n- - - - - - - -")
+
+    check(2, 5)
+    check(2, 2)
+    check(5, 2)
 }
