2d0da94ceefb9600a8fa5355e49e09bfd6036555
Milos Vasic <milos85vasic@gmail.com>
2021-02-22 10:38:46 +0100

    Fundamental Kotlin - Generic functions.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/generics/Functions.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/generics/Functions.kt
@@ -3,25 +3,31 @@ package net.milosvasic.fundamental.kotlin.object_oriented.generics
 fun main() {
 
     abstract class Engine {
+
         abstract val power: Long
 
         override fun toString(): String {
+
             return "Engine(power=$power)"
         }
     }
 
     class RocketEngine : Engine() {
+
         override val power: Long
             get() = 1000
     }
 
     class TruckEngine : Engine() {
+
         override val power: Long
             get() = 100
     }
 
     class EngineDiagnostics {
+
         fun <T : Engine> checkEngine(engine: T) {
+
             println(engine)
         }
     }
