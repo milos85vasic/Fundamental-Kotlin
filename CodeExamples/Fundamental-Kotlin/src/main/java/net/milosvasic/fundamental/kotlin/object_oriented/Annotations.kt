6c732c8001d40cfa6eb6a4b888e43f0dc98c933e
Milos Vasic <milos85vasic@gmail.com>
2021-02-27 11:51:09 +0100

    Fundamental Kotlin - Annotations.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Annotations.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Annotations.kt
@@ -1,5 +1,23 @@
 package net.milosvasic.fundamental.kotlin.object_oriented
 
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.VALUE_PARAMETER,
+    AnnotationTarget.EXPRESSION
+)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class Marker
+
+@Marker class Annotated {
+
+    @Marker fun hello(@Marker who: String): String {
+
+        return (@Marker "Hello $who")
+    }
+}
+
 fun main() {
 
 
