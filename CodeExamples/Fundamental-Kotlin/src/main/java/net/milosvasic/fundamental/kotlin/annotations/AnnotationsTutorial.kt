119427151e0d06fed1c28da5d6e7c6dad0b48933
Milos Vasic <milos85vasic@gmail.com>
2021-03-05 10:48:14 +0100

    Fundamental Kotlin - Annotations tutorial.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/annotations/AnnotationsTutorial.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/annotations/AnnotationsTutorial.kt
@@ -1,6 +1,35 @@
 package net.milosvasic.fundamental.kotlin.annotations
 
+@Target(
+    AnnotationTarget.FIELD,
+    AnnotationTarget.LOCAL_VARIABLE
+)
+annotation class Descriptor(val description: String)
+
 fun main() {
 
+    class Product(val id: Long)
 
+    class Processor {
+
+        fun process(products: List<Product>) {
+
+            products.forEach {
+
+                println("Processing product, ID: ${it.id}")
+                val ann = it::class.annotations
+                println(ann)
+            }
+        }
+    }
+
+    var id = 0L
+
+    @Descriptor(description = "The Car")
+    val car = Product(++id)
+
+    val products = listOf(car)
+    val processor = Processor()
+
+    processor.process(products)
 }
