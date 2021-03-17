3f518a5d0ad58696378cadd9b7b4758d514a6af2
Milos Vasic <milos85vasic@gmail.com>
2021-03-17 10:55:49 +0100

    Fundamental Kotlin - Extending objects.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/ExtendingObjects.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/ExtendingObjects.kt
@@ -1,6 +1,36 @@
 package net.milosvasic.fundamental.kotlin.object_oriented.extensions
 
+interface Drawing {
+
+    fun draw()
+}
+
+class Objects {
+
+    val CIRCLE = object : Drawing {
+
+        override fun draw() {
+
+            println("Circle")
+        }
+    }
+}
+
+val Objects.SQUARE: Drawing
+    get() = object : Drawing {
+
+        override fun draw() {
+
+            println("Square")
+        }
+    }
+
 fun main() {
 
+    val o = Objects()
+    with(o) {
 
+        CIRCLE.draw()
+        SQUARE.draw()
+    }
 }
