0ee6503dc1db6d328db2ebc4773e304ec2acf549
Milos Vasic <milos85vasic@gmail.com>
2021-03-18 12:03:49 +0100

    Fundamental Kotlin - Android extension example.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/AndroidExtension.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/AndroidExtension.kt
@@ -1,8 +1,9 @@
 package net.milosvasic.fundamental.kotlin.object_oriented.extensions
 
-fun Activity.goBack() {
-    // We will pop back stack here ...
-}
-
 // Mock for Android activity
 class Activity
+
+fun Activity.goBack() {
+
+    // We will pop back stack here ...
+}
