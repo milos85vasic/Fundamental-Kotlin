d5d33d2e567d18e521c33cd1b5396ef58664a90f
Milos Vasic <milos85vasic@gmail.com>
2021-03-19 16:06:34 +0100

    Fundamental Kotlin - Extending function literals.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/ExtendingFunctionLiterals.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/extensions/ExtendingFunctionLiterals.kt
@@ -2,5 +2,13 @@ package net.milosvasic.fundamental.kotlin.object_oriented.extensions
 
 fun main() {
 
+    class Printer() {
+
+        fun print(what: String) {
+
+            println(what)
+        }
+    }
+
 
 }
