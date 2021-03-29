eb639d24f4c9d85e0fabeafc95f8dd207655cced
Milos Vasic <milos85vasic@gmail.com>
2021-03-29 16:58:25 +0200

    Fundamental Kotlin - Scope functions - Take If / Unless.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/scope_functions/TakeIfUnless.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/scope_functions/TakeIfUnless.kt
@@ -2,5 +2,18 @@ package net.milosvasic.fundamental.kotlin.scope_functions
 
 fun main() {
 
+    val numbers = listOf(-3, -2, -1, 0, 1, 2, 3)
 
+    numbers.forEach {
+
+        val notZero = it.takeUnless { it == 0 }
+        notZero?.let { nZero ->
+
+            val positive = nZero.takeIf { it > 0 }
+            positive?.let { pos ->
+
+                println("Positive, non zero number: $pos")
+            }
+        }
+    }
 }
