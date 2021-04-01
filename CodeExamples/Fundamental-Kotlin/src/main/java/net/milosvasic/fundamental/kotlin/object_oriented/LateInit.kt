30bff0649bfa3979aae37f790522c78411cd2950
Milos Vasic <milos85vasic@gmail.com>
2021-04-01 14:30:55 +0200

    Fundamental Kotlin - Late init.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/LateInit.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/LateInit.kt
@@ -1,16 +1,19 @@
 package net.milosvasic.fundamental.kotlin.object_oriented
 
-class SomeExampleClass {
-    fun printMe() {
-        println("I am late initialized!")
+fun main() {
+
+    class SomeExampleClass {
+        fun printMe() {
+            println("I am late initialized!")
+        }
     }
-}
 
-class Late {
-    lateinit var x: SomeExampleClass
+    class Late {
+        lateinit var x: SomeExampleClass
 
-    fun printMe() {
-        x = SomeExampleClass()
-        x.printMe()
+        fun printMe() {
+            x = SomeExampleClass()
+            x.printMe()
+        }
     }
 }
