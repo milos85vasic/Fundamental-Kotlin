9101d7844b475ff23472e74a005d3daa9c002867
Milos Vasic <milos85vasic@gmail.com>
2021-04-02 15:40:33 +0200

    Fundamental Kotlin - Nested classes.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Nested.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Nested.kt
@@ -1,21 +1,22 @@
 package net.milosvasic.fundamental.kotlin.object_oriented
 
-/**
- * Created by mvasic on 7/2/16.
- */
 class Nested1 {
+
     val a = 0
 
     class Nested2 {
+
         val a = 1
 
         class Nested3 {
+
             val a = 2
         }
     }
 }
 
-fun tryNested() {
+fun main() {
+
     val n1 = Nested1()
     val n2 = Nested1.Nested2()
     val n3 = Nested1.Nested2.Nested3()
