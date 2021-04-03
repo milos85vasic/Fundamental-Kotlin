8fd3a3446ba0978e1e1230bddcf10ef9e8a2682d
Milos Vasic <milos85vasic@gmail.com>
2021-04-03 14:32:59 +0200

    Fundamental Kotlin - Inner classes.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Inner.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/object_oriented/Inner.kt
@@ -1,17 +1,17 @@
 package net.milosvasic.fundamental.kotlin.object_oriented
 
-/**
- * Created by mvasic on 7/2/16.
- */
-class InnerExample {
-    val a = 100
+fun main() {
 
-    inner class Inner {
-        val b = a
+    class InnerExample {
+
+        val a = 100
+
+        inner class Inner {
+
+            val b = a
+        }
     }
-}
 
-fun tryInner() {
     val x = InnerExample().Inner()
     println("Value of b is: ${x.b}")
 }
