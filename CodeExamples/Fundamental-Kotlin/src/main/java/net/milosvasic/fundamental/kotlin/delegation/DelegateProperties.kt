64c199fe6c18d70d420eccfd1b97b4f148a5a52c
Milos Vasic <milos85vasic@gmail.com>
2021-04-06 16:03:59 +0200

    Fundamental Kotlin - Delegating properties.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/delegation/DelegateProperties.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/delegation/DelegateProperties.kt
@@ -3,29 +3,34 @@ package net.milosvasic.fundamental.kotlin.delegation
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class ToBeDelegated() {
-    var value: Int by Delegator()
-}
+fun main() {
 
-/*
- * ReadWriteProperty:
- * Base interface that can be used for implementing property delegates of read-write properties.
- */
-class Delegator() : ReadWriteProperty<Any, Int> {
-    var value = 0
+    /*
+     * ReadWriteProperty:
+     * Base interface that can be used for implementing property delegates of read-write properties.
+     */
+    class Delegator : ReadWriteProperty<Any, Int> {
 
-    override fun getValue(thisRef: Any, property: KProperty<*>): Int {
-        println("Value [ GET ][ ${property.name} ]")
-        return value
+        var value = 0
+
+        override fun getValue(thisRef: Any, property: KProperty<*>): Int {
+
+            println("Value [ GET ][ ${property.name} ]")
+            return value
+        }
+
+        override fun setValue(thisRef: Any, property: KProperty<*>, value: Int) {
+
+            println("Value [ SET ][ ${property.name} ][ ${value} ]")
+            this.value = value
+        }
     }
 
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: Int) {
-        println("Value [ SET ][ ${property.name} ][ ${value} ]")
-        this.value = value
-    }
-}
+    class ToBeDelegated {
 
-fun tryPropertiesDelegate() {
+        var value: Int by Delegator()
+    }
+
     val x = ToBeDelegated()
     val y = ToBeDelegated()
     val z = ToBeDelegated()
