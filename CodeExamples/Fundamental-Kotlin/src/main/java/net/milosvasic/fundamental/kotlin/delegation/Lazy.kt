6faee05dabe6a1f338e51f7b66c21dcc1bab3708
Milos Vasic <milos85vasic@gmail.com>
2021-04-07 13:05:59 +0200

    Fundamental Kotlin - Lazy initialization.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/delegation/Lazy.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/delegation/Lazy.kt
@@ -1,18 +1,20 @@
 package net.milosvasic.fundamental.kotlin.delegation
 
-/**
- * Created by mvasic on 7/19/16.
- */
-class UsesLazy {
-    val myLazyValue: String by lazy {
-        println("I am initializing this lazy value!")
-        "My lazy value!"
-    }
-}
 
-fun useLazy(){
-    val usesLazy: UsesLazy = UsesLazy()
-    val a: String = usesLazy.myLazyValue
-    val b: String = usesLazy.myLazyValue
-    val c: String = usesLazy.myLazyValue
+fun main() {
+
+    class Example {
+
+        val hello: String by lazy {
+
+            println("I am initializing this lazy value")
+            "Hello!"
+        }
+    }
+
+    val example = Example()
+
+    val a = example.hello
+    val b = example.hello
+    val c = example.hello
 }
