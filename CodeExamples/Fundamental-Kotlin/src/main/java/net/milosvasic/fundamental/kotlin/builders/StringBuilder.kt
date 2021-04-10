24fc0e67d3d9932cb6082123575bdf4067cdc290
Milos Vasic <milos85vasic@gmail.com>
2021-04-10 13:25:42 +0200

    Fundamental Kotlin - String builder.

--- CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/builders/StringBuilder.kt
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/builders/StringBuilder.kt
@@ -2,5 +2,22 @@ package net.milosvasic.fundamental.kotlin.builders
 
 fun main() {
 
+    val builder = StringBuilder()
+        .append("Hello world!")
+        .append("\n")
+        .append("Numbers: ")
 
+    val numbers = StringBuilder()
+    for (number in 1 until 3) {
+
+        numbers.append(number)
+            .append(" ")
+    }
+
+    val text = builder.append(numbers)
+        .append("\n")
+        .append("Lore ipsum...")
+        .toString()
+
+    println(text)
 }
