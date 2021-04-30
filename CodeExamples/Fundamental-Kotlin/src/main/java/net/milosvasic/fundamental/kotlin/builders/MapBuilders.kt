861cbff5d327d84e7559fe0842661d1407abef19
Milos Vasic <milos85vasic@gmail.com>
2021-04-30 16:03:01 +0200

    Fundamental Kotlin - Map builders.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/builders/MapBuilders.kt
@@ -0,0 +1,15 @@
+package net.milosvasic.fundamental.kotlin.builders
+
+fun main(){
+
+    val map1 = mapOf("hello" to "world", "lorem" to "ipsum", "abc" to "def")
+
+    val map2 = mutableMapOf("hello" to "world", "lorem" to "ipsum")
+    map2["abc"] = "def"
+
+    val map3 = sortedMapOf("hello" to "world", "lorem" to "ipsum", "abc" to "def")
+    val map4 = hashMapOf("hello" to "world", "lorem" to "ipsum", "abc" to "def")
+    val map5 = linkedMapOf("hello" to "world", "lorem" to "ipsum", "abc" to "def")
+
+
+}
