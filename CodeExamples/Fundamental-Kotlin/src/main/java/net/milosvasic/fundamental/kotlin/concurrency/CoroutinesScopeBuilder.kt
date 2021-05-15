bf0c88f0a287f7dfefa0d91446c899289aa48427
Milos Vasic <milos85vasic@gmail.com>
2021-05-15 16:05:43 +0200

    Fundamental Kotlin - Coroutines scope builder.

---
+++ CodeExamples/Fundamental-Kotlin/src/main/java/net/milosvasic/fundamental/kotlin/concurrency/CoroutinesScopeBuilder.kt
@@ -0,0 +1,30 @@
+package net.milosvasic.fundamental.kotlin.concurrency
+
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+
+    fire()
+    println("Completed")
+}
+
+// 'suspend' means that coroutine can be suspended for the later execution
+suspend fun fire() = coroutineScope {
+
+    for (x in 0..3) {
+
+        launch {
+
+            for (y in 0..5) {
+
+                delay(1000L)
+                println("Job $x, count no. $y at ${System.currentTimeMillis()}")
+            }
+        }
+    }
+
+    println("Started")
+}
