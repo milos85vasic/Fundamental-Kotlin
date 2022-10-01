efa1f7ce391af7e2f3ecd58d5ae003e88fb80322
Milos Vasic <i@mvasic.ru>
2022-10-01 16:41:59 +0300

    The 'Upstreamable' support.

---
+++ Upstreams/GitFlic.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@gitflic.ru:milosvasic/fundamental-kotlin.git"
---
+++ Upstreams/GitHub.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export UPSTREAMABLE_REPOSITORY="git@github.com:milos85vasic/Fundamental-Kotlin.git"
